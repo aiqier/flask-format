51afca41127706c6efe9ceb355526aab6adb1103
aiqier <liuaiqi627@gmail.com>
2015-11-22 23:44:07 +0800

    日常提交

--- app/utils/check.py
+++ app/utils/check.py
@@ -13,3 +13,48 @@
 返回: 成功+None, 失败+错误原因
 
 """
+
+def is_int(i, n):
+    """
+    判断是否为整数
+    :param i: 变量
+    :param n: 变量名
+    """
+    if isinstance(i, int):
+        return True, None
+    else:
+        return False, u"{}非整数".format(n)
+
+def is_str(s, n):
+    """
+    判断是否为字符串
+    """
+    if isinstance(s, (str, unicode)):
+        return True, None
+    else:
+        return False, u"{}非字符串".format(n)
+
+def is_legal_amt(a, n):
+    """
+    判断是否合法金额
+    """
+    if a > 0:
+        return True, None
+    else:
+        return False, u"{}:{}非法金额".format(n, a)
+
+def is_legal_bank_card(s, n):
+    """
+    判断是否是合法银行卡
+    """
+    if n.strip() == "" or len(s) > 20:
+        return False, u"{}:{}非法银行卡".format(n, s)
+    return True, None
+
+def is_legal_digit(s, n):
+    """
+    判断是否是合法数字字符串
+    """
+    if n.isdigit():
+        return True, None
+    return False, u"{}:{}非法数字字符串".format(n, s)
